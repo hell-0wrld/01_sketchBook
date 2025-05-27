5e1dfdb0d8884b255ba18759c48bc646060204ca
hell-0wrld <crayonshiin@gmail.com>
2025-05-27 13:52:47 +0900

    해치웠나?

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from subtract import subtract
 from multiply import multiply
 from divide import divide
 def calculator():
-    a = float(input("첫 번째 숫자: "))
-    b = float(input("두 번째 숫자: "))
+    a = float(input("첫 번째 숫자 넣어주삼!!!: "))
+    b = float(input("두 번째 숫자도 넣어주삼!!!: "))
     op = input("연산자 (+, -, *, /): ")
     operations = {'+': add,
                   '-': subtract,
@@ -15,7 +15,8 @@ def calculator():
             result = operations[op](a, b)
             print("결과:", result)
         except Exception as e: print("오류:", e)
-    else: print("올바르지 않은 연산자입니다.")
+    else: print("올바르지 않은 연산자임;;;;?;")
+
 
 if __name__ == "__main__":
     calculator()
